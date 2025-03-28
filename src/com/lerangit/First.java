c694c3cf698b09077d83eae9e1f487d9b7612086
ramswaroop1 <ramswaroop@coedify.com>
2025-03-28 18:21:07 +0530

    sort array

---
+++ src/com/lerangit/First.java
@@ -0,0 +1,32 @@
+package com.lerangit;
+
+public class First {
+	public static void sortArray(int arr[]) {
+		int minIndex = -1;
+		for(int i =0; i<arr.length-1; i++) {
+			int min = arr[i];
+			boolean flag = false;
+			for(int j =i+1; j<arr.length; j++) {
+				if(arr[j]<min) {
+					min = arr[j];
+					minIndex = j;
+					flag = true;
+				}
+			}
+			if(flag) {
+				int temp = arr[minIndex];
+				arr[minIndex] = arr[i];
+				arr[i] = temp;
+			}	
+		}
+		for(int i : arr) {
+			System.out.print(i+" ");
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[]= {1};
+		sortArray(arr);
+	}
+
+}
